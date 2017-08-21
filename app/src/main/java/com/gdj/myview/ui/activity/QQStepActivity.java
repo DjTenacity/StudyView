dae85c0c285734993f8cced6920d5d5df9dd7fb9
gdjsdk <1757286697@qq.com>
2017-08-21 18:40:44 +0800

    snackbar

--- app/src/main/java/com/gdj/myview/ui/activity/QQStepActivity.java
+++ app/src/main/java/com/gdj/myview/ui/activity/QQStepActivity.java
@@ -2,8 +2,10 @@ package com.gdj.myview.ui.activity;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.ListPopupWindow;
@@ -52,7 +54,8 @@ public class QQStepActivity extends AppCompatActivity {
 
     private String[] mItems = {"1311", "223222", "1131", "222322", "1131", "222232",
             "1411", "1131", "222322", "1131", "222232", "22222", "11133", "22222"};
-    private HashMap<Double, Double> map   = new HashMap<>();;
+    private HashMap<Double, Double> map = new HashMap<>();
+    ;
 
 
     @Override
@@ -60,9 +63,9 @@ public class QQStepActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         ButterKnife.bind(this);
-     //   menu_chart= (BrokenView) findViewById(R.id.menu_chart);
+        //   menu_chart= (BrokenView) findViewById(R.id.menu_chart);
 
-     //   mSingleLineFlowLayout = (SingleLineFlowLayout) findViewById(R.id.single_line_layout);
+        //   mSingleLineFlowLayout = (SingleLineFlowLayout) findViewById(R.id.single_line_layout);
 
         mSingleLineFlowLayout.post(new Runnable() {
             @Override
@@ -75,19 +78,19 @@ public class QQStepActivity extends AppCompatActivity {
                 }
             }
         });
-        map.put(0.0,0.0);
-        map.put(1.0,3.0);
-        map.put(2.0,13.0);
-        map.put(3.0,23.0);
-        map.put(4.0,33.0);
-        map.put(5.0,14.0);
-        map.put(6.0,46.0);
-        map.put(7.0,36.0);
-        map.put(8.0,33.0);
+        map.put(0.0, 0.0);
+        map.put(1.0, 3.0);
+        map.put(2.0, 13.0);
+        map.put(3.0, 23.0);
+        map.put(4.0, 33.0);
+        map.put(5.0, 14.0);
+        map.put(6.0, 46.0);
+        map.put(7.0, 36.0);
+        map.put(8.0, 33.0);
 
 //       menu_chart.setView(map,50,10,20,50,"s","b" );
 
-        DroidCardsView deddd=new DroidCardsView(this);
+        DroidCardsView deddd = new DroidCardsView(this);
         ll.addView(deddd);
     }
 
@@ -124,9 +127,9 @@ public class QQStepActivity extends AppCompatActivity {
         showListPop(btn);
     }
 
-    void showListPop(View view){
-        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mItems);
-        final ListPopupWindow listPopupWindow=new ListPopupWindow(this);
+    void showListPop(View view) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItems);
+        final ListPopupWindow listPopupWindow = new ListPopupWindow(this);
         listPopupWindow.setAdapter(adapter);
         listPopupWindow.setAnchorView(view);
         listPopupWindow.setHeight(500);
@@ -137,24 +140,68 @@ public class QQStepActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 listPopupWindow.dismiss();
-                Toast.makeText(QQStepActivity.this, "ListPopupWindow"+position, Toast.LENGTH_SHORT).show();
-
+                myToast("ListPopupWindow" + position);
             }
         });
         listPopupWindow.show();
     }
 
-    public void showPopupMenu(View view){
-        PopupMenu popup=new PopupMenu(this,view);
-        popup.getMenuInflater().inflate(R.menu.menu_web,popup.getMenu());
+    public void showPopupMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.getMenuInflater().inflate(R.menu.menu_web, popup.getMenu());
         popup.setGravity(Gravity.CENTER);
         popup.show();
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
+                myToast(item.getTitle().toString());
                 return false;
             }
         });
     }
+
+    void myToast(String str) {
+        Toast result = new Toast(this);
+//com.android.internal.R.layout.transient_notification
+        LayoutInflater inflate = (LayoutInflater)
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflate.inflate(R.layout.item_main_type, null);
+        TextView tv = (TextView) v.findViewById(R.id.divider);
+        tv.setText("自定义吐司" + str);
+
+        result.setView(v);
+        result.setDuration(Toast.LENGTH_SHORT);
+        result.show();
+    }
+
+    public void showMySnackbar(final View view) {
+        myToast("\"toast和snackbar\"");
+
+        //Snackbar.LENGTH_INDEFINITE   无穷
+        //传入view 是因为 snackbar 要依附于某一个parentView
+        Snackbar snackbar=Snackbar.make(view,"只能一个Action,要不要隐藏",Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("确定隐藏", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(view);
+            }
+        });
+
+        snackbar.setCallback(new Snackbar.Callback(){
+            @Override
+            public void onShown(Snackbar sb) {
+                // Stub implementation to make API check happy.
+            }
+            @Override
+            public void onDismissed(Snackbar transientBottomBar, @DismissEvent int event) {
+                // Stub implementation to make API check happy.
+
+            }
+        });
+
+        snackbar.show();
+
+    }
+
 
 }
